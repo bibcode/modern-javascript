987c2330986ebbf112e4d64c78abd0454906859e
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-08-29 10:25:34 +0100

    array practice

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 const array =['banana', 'Apple', 'watermelon', 'orange', 'strawberry', 'grapes']
 
-const strings =array.map((fruit)=>{
-    const upperCase = fruit.toUpperCase('fruit')
-    console.log(upperCase);
-})
+// const strings =array.map((fruit)=>{
+//     const upperCase = fruit.toUpperCase('fruit')
+//     console.log(upperCase);
+// })
 // console.log(strings);
 // console.log(array);
 // const arrayMap =array.filter((fruit)=>{
@@ -11,3 +11,15 @@ const strings =array.map((fruit)=>{
 // })
 
 
+const myFruits =['banana', 'apple', 'watermelon', 'tomatoes']
+// console.log(myFruits);
+// const strings =myFruits.filter((fruit)=>{
+//     const myFilter = fruit.valueOf()
+//     console.log(myFilter);
+// })
+
+const strings = myFruits.map((fruit)=>{
+    if(fruit.length > 0){
+        
+    }
+})
